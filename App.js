a0a2c46e29ae04d8676ccac78a622df9444769d2
piyashah123 <82143407+piyashah123@users.noreply.github.com>
2022-10-25 12:58:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fontLoaded: false
+
+    }
+
+  }
+  async loadFonts() {
+    await Font.loadAsync({
+      Rajdhani_600SemiBold: Rajdhani_600SemiBold
+    })
+    this.setState({ fontLoaded: true })
+  }
+  componentDidMount(){
+    this.loadFonts();
+    
+  }
+  render() {
+    const {fontLoaded}=this.state;
+    if (fontLoaded){
+    return <BottomTabNavigator />;}
+  }
+}
